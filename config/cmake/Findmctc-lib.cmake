73ec7cd48037303e44a934096cc775a03f10b626
Timofei Zakharov <timchem1508@gmail.com>
2026-03-03 16:34:48 +0100

    Update mctc-lib dependency to use the 'timer' tag

--- config/cmake/Findmctc-lib.cmake
+++ config/cmake/Findmctc-lib.cmake
@@ -17,7 +17,7 @@
 set(_lib "mctc-lib")
 set(_pkg "MCTCLIB")
 set(_url "https://github.com/timchem1508/mctc-lib_timer.git")
-set(_rev "main")
+set(_rev "timer")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
